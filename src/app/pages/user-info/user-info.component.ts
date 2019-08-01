2608838a4569d4f8b3346cda96d28b4a5f543771
kanazawanao <naomiti3333@gmail.com>
2019-08-01 17:28:07 +0900

    ユーザー情報更新処理実装

--- src/app/pages/user-info/user-info.component.ts
+++ src/app/pages/user-info/user-info.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../../services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss']
 })
 export class UserInfoComponent implements OnInit {
-  constructor(public auth: AuthService) {}
+  user:User = {
+    uid: '',
+    displayName: '',
+    email: '',
+    photoURL: '',
+  }
+  constructor(
+    private auth: AuthService,
+    private userService: UserService) {}
 
   ngOnInit() {
-    this.auth.user.subscribe(user => {
-      console.log(user);
+    this.auth.user.subscribe(u => {
+      this.user = u;
     });
+  }
+
+  update() {
+    console.log(this.user);
+    this.userService.updateUserData(this.user);
   }
 }
