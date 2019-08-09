09d7384d63aae694677bffa6bd559cc616309199
kanazawanao <naomiti3333@gmail.com>
2019-08-09 13:15:38 +0900

    setting category complete close #4

--- src/app/parts/select-category/select-category.component.ts
+++ src/app/parts/select-category/select-category.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { CATEGORIES, Category } from './categories';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Place } from 'src/app/models/place';
+import { Category } from 'src/app/models/category';
+import { Observable, Subscription } from 'rxjs';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-select-category',
   templateUrl: './select-category.component.html',
   styleUrls: ['./select-category.component.scss']
 })
-export class SelectCategoryComponent implements OnInit {
+export class SelectCategoryComponent implements OnInit, OnDestroy {
   @Input() place: Place = new Place();
-  categories: Category[] = CATEGORIES;
-  constructor() {}
+  subscriptions: Subscription[] = [];
+  categories$?: Observable<Category | undefined>;
+  categories: Category =  new Category;
+  constructor(private categoryService: CategoryService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.categories$ = this.categoryService.getCategories();
+    this.subscriptions.push(this.categories$.subscribe(c => this.categories = c ? c : new Category));
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
 }
