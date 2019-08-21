ec01611731e72945dcca22b2c6471d6113ea2b1d
kanazawanao <naomiti3333@gmail.com>
2019-08-21 18:01:48 +0900

    update user group #13

--- src/app/models/place.ts
+++ src/app/models/place.ts
@@ -3,12 +3,12 @@ import { UserGroup } from './user-group';
 export class Place {
   id = '';
   uId = '';
+  gid = '';
   place = '';
   prefecture = '';
   addr = '';
   went = false;
   price = 0;
-  userGroup: UserGroup = new UserGroup();
   category: string[] = [];
   open?: Date;
   close?: Date;
