e9992cb968531a7f46ffbe7667f9f6bdeeb65618
kanazawanao <naomiti3333@gmail.com>
2019-08-26 11:57:56 +0900

    environment modify

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "<api-key>",
-    authDomain: "<auth-domain>",
-    databaseURL: "<database-url>",
-    projectId: "<project-id>",
-    storageBucket: "",
-    messagingSenderId: "<messaging-sender-id>",
-    appId: "<app-id>"
+    apiKey: '<api-key>',
+    authDomain: '<auth-domain>',
+    databaseURL: '<database-url>',
+    projectId: '<project-id>',
+    storageBucket: '',
+    messagingSenderId: '<messaging-sender-id>',
+    appId: '<app-id>'
   },
   dartskyApiKey: '<dark-sky-api-key>',
   weatherNewsUrl: '<weather-news-url>'
